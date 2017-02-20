951b69207599b98b2a330f61457de2e652fba151
Raphael Carubbi Neto <rcarubbi@gmail.com>
2017-02-20 18:25:24 -0300

    projeto de signalR

---
+++ TemplatePadrao/SignalRChat/Models/ChatHub.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRChat.Models
+{
+    public class ChatHub : Hub
+    {
+        public void EnviarMensagem(string apelido, string mensagem)
+        {
+            Clients.All.TransmitirMensagem(apelido, mensagem);
+        }
+    }
+}
