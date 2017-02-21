e855e6e738e083529fcdbf8c3f79efd780478c21
Raphael <Raphael@CARUBBI>
2017-02-21 18:05:22 -0300

    signalR Funcionando

---
+++ TemplatePadrao/SignalRChat/App_Start/Startup.SignalR.cs
@@ -0,0 +1,14 @@
+﻿
+using Owin;
+
+namespace SignalRChat
+{
+    public partial class Startup
+    {
+     
+        public void ConfigureSignalR(IAppBuilder app)
+        {
+            app.MapSignalR();
+        }
+    }
+}
---
+++ TemplatePadrao/SignalRChat/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SignalRChat.Startup))]
+
+namespace SignalRChat
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureSignalR(app);
+        }
+    }
+}
